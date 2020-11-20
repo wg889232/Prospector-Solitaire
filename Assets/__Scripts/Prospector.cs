b85a1bac15d413ee43049f301659d7e9785cc439
Wyatt Greenman <wygreenman@gmail.com>
2020-11-19 21:38:22 -0500

    Finished Book Game
    Finished Book Game

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -17,6 +17,8 @@ public class Prospector : MonoBehaviour
     public Vector2 fsPosRun = new Vector2(0.5f, 0.75f);
     public Vector2 fsPosMid2 = new Vector2(0.4f, 1.0f);
     public Vector2 fsPosEnd = new Vector2(0.5f, 0.95f);
+    public float reloadDelay = 2f;
+    public Text gameOverText, roundResultText, highScoreText;
     
     [Header("Set Dynamically")]
     public Deck deck;
@@ -31,6 +33,39 @@ public class Prospector : MonoBehaviour
     void Awake()
     {
         S = this;
+        SetUpUITexts();
+    }
+
+    void SetUpUITexts()
+    {
+        GameObject go = GameObject.Find("HighScore");
+        if(go != null)
+        {
+            highScoreText = go.GetComponent<Text>();
+        }
+        int highScore = ScoreManager.HIGH_SCORE;
+        string hScore = "High Score: " + Utils.AddCommasToNumber(highScore);
+        go.GetComponent<Text>().text = hScore;
+
+        go = GameObject.Find("GameOver");
+        if(go != null)
+        {
+            gameOverText = go.GetComponent<Text>();
+        }
+
+        go = GameObject.Find("RoundResult");
+        if(go != null)
+        {
+            roundResultText = go.GetComponent<Text>();
+        }
+
+        ShowResultsUI(false);
+    }
+
+    void ShowResultsUI(bool show)
+    {
+        gameOverText.gameObject.SetActive(show);
+        roundResultText.gameObject.SetActive(show);
     }
 
     // Start is called before the first frame update
@@ -244,19 +279,36 @@ public class Prospector : MonoBehaviour
 
     void GameOver(bool won)
     {
+        int score = ScoreManager.SCORE;
+        if (fsRun != null) score += fsRun.score;
         if (won)
         {
+            gameOverText.text = "Round Over";
+            roundResultText.text = "You won this round!\nRound Score: " + score;
+            ShowResultsUI(true);
             //print("GameOver. You won! :)");
             ScoreManager.EVENT(eScoreEvent.gameWin);
             FloatingScoreHandler(eScoreEvent.gameWin);
         }
         else
         {
+            gameOverText.text = "Game Over";
+            if(ScoreManager.HIGH_SCORE <= score)
+            {
+                string str = "You got the high score!\nHigh score: " + score;
+                roundResultText.text = "Your final score was: " + score;
+            }
+            ShowResultsUI(true);
             //print("Game Over. You Lost. :(");
             ScoreManager.EVENT(eScoreEvent.gameLose);
             FloatingScoreHandler(eScoreEvent.gameLose);
         }
 
+        Invoke("ReloadLevel", reloadDelay);
+    }
+
+    void ReloadLevel()
+    {
         SceneManager.LoadScene("__Prospector_Scene_0");
     }
 
